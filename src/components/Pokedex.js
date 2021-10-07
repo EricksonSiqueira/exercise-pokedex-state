9e2accfe3198fec51a30ecf8012cc28c53aed7b0
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-07 19:33:25 -0300

    Implementando logica do botao de proximo pokemon

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -4,17 +4,39 @@ import pokemons from '../data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pokemonIndex: 0,
+    }
+
+    this.nextPokemon = this.nextPokemon.bind(this);
+  }
+
+  nextPokemon() {
+    if(this.state.pokemonIndex === pokemons.length - 1){
+      this.setState((_previousValue, _props) => ({
+        pokemonIndex: 0,
+      }))
+    } else {
+      this.setState((previousValue, _props) => ({
+        pokemonIndex: previousValue.pokemonIndex + 1,
+      }))
+    }
+  }
   render() {
+    const {pokemonIndex} = this.state;
     return(
       <div className="pokedex">
         <header>
           <h1>Pokedex</h1>
         </header>
         <main className="pokedex-cards">
-          <Pokemon key={pokemons[0].id} pokemonData={pokemons[0]} />
+          <Pokemon key={pokemons[pokemonIndex].id} pokemonData={pokemons[pokemonIndex]} />
         </main>
         <section>
-          <button className="next-btn">Próximo</button>
+          <button onClick={this.nextPokemon} className="next-btn">Próximo</button>
         </section>
       </div>
     );
