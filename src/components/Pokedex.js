0f6cd726cb304c8a5f676a35f10bfd99e4bc049b
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-07 21:54:06 -0300

    Implementando filtro de pesquisa por tipo de pokemon

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -9,34 +9,54 @@ class Pokedex extends Component {
 
     this.state = {
       pokemonIndex: 0,
-      pokemonFilter: 'fire',
+      pokemonArr: pokemons,
     }
     this.setPokemonFilter = this.setPokemonFilter.bind(this);
     this.nextPokemon = this.nextPokemon.bind(this);
+    this.resetPokemonIndex = this.resetPokemonIndex.bind(this)
   }
 
-  setPokemonFilter() {
+  resetPokemonIndex() {
+    this.setState((_previousValue, _prop) => ({
+      pokemonIndex: 0,
+    }))
+  }
 
+  setPokemonFilter({target}) {
+    this.resetPokemonIndex();
+    const filteredPokemons = pokemons.filter((pokemon) => pokemon.type === target.value)
+    this.setState(() => ({
+      pokemonArr: filteredPokemons,
+    }))
   }
   nextPokemon() {
-    const lastIndex = pokemons.length - 1;
-    const { pokemonIndex } = this.state;
+    const { pokemonIndex, pokemonArr } = this.state;
+    const lastIndex = pokemonArr.length - 1;
       this.setState((previousValue, _props) => ({
         pokemonIndex: (pokemonIndex === lastIndex) ? 0 : previousValue.pokemonIndex + 1,
       }))
   }
   render() {
-    const {pokemonIndex} = this.state;
+    const {pokemonIndex, pokemonArr} = this.state;
     return(
       <div className="pokedex">
         <header>
           <h1>Pokedex</h1>
         </header>
         <main className="pokedex-cards">
-          <Pokemon key={pokemons[pokemonIndex].id} pokemonData={pokemons[pokemonIndex]} />
+          <Pokemon key={pokemonArr[pokemonIndex].id} pokemonData={pokemonArr[pokemonIndex]} />
         </main>
         <section>
           <button onClick={this.nextPokemon} className="next-btn">Próximo</button>
+        </section>
+        <section>
+          <button onClick={this.setPokemonFilter} value="Fire" className="next-btn">Fire</button>
+          <button onClick={this.setPokemonFilter} value="Electric" className="next-btn">Electric</button>
+          <button onClick={this.setPokemonFilter} value="Bug" className="next-btn">Bug</button>
+          <button onClick={this.setPokemonFilter} value="Poison" className="next-btn">Poison</button>
+          <button onClick={this.setPokemonFilter} value="Psychic" className="next-btn">Psychic</button>
+          <button onClick={this.setPokemonFilter} value="Normal" className="next-btn">Normal</button>
+          <button onClick={this.setPokemonFilter} value="Dragon" className="next-btn">Dragon</button>
         </section>
       </div>
     );
